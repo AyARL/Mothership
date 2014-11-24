59e22a388d79de5f57ac46a689329e6afeed5e42
IDCs <idc.dts@gmail.com>
2014-11-24 17:50:07 +0000

    Added Pathfinding.

---
+++ Mothership/Assets/Scripts/AI/CDroneAI.cs
@@ -0,0 +1,169 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Mothership;
+
+public class CDroneAI : MonoBehaviour {
+	
+    // Declares the drone's states.
+	public enum EDroneState
+	{
+		DRONE_IDLE,
+		DRONE_MOVING,
+        DRONE_ATTACKING,
+        DROME_DEAD,
+	}
+	
+    // Drone state should be idle by default
+    [ SerializeField ]
+    private EDroneState m_eState = EDroneState.DRONE_IDLE;
+    public EDroneState DroneState { get { return m_eState; } }
+
+    // The speed of this lovely drone.
+    [ SerializeField ]
+	private float m_fSpeed;
+    public float Speed { get { return m_fSpeed; } }
+
+    // The Health.
+    [ SerializeField ]
+    private float m_fHealth = 100;
+    public float Health { get { return m_fHealth; } }
+
+	public bool DebugMode;
+	
+    // Will indicate if we reached the target node.
+	private bool m_bReachedNode = true;
+    public bool ReachedNode { get { return m_bReachedNode; } }
+
+    // Current target position.
+    [ SerializeField ]
+	private Vector3 m_v3Target = Vector3.zero;
+    public Vector3 TargetPosition { get { return m_v3Target; } }
+
+    // Target Node.
+	Vector3 m_v3CurrNode;
+	int m_iNodeIndex;
+	List< Vector3 > m_liPath = new List<Vector3>();
+	float m_fOldTime = 0;
+	float m_fCheckTime = 0;
+	float m_fElapsedTime = 0;
+
+    void Awake()
+    {
+        m_v3Target = GameObject.Find( "Target" ).transform.position;
+    }
+	
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               Update
+    /////////////////////////////////////////////////////////////////////////////
+	void Update () 
+	{
+        // Will run the NPCs State machines logic.
+        RunStates();
+	}
+
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               RunStates
+    /////////////////////////////////////////////////////////////////////////////
+    private void RunStates()
+    {
+        // Set the NPCs speed.
+		m_fSpeed = Time.deltaTime * Constants.DEFAULT_NPC_SPEED;
+		m_fElapsedTime += Time.deltaTime;
+		
+		if (m_fElapsedTime > m_fOldTime)
+		{
+			switch ( m_eState )
+			{
+			case EDroneState.DRONE_IDLE:
+
+                    SetTarget();
+
+				break;
+				
+			case EDroneState.DRONE_MOVING:
+				m_fOldTime = m_fElapsedTime + 0.01f;
+
+				if (m_fElapsedTime > m_fCheckTime)
+				{
+					m_fCheckTime = m_fElapsedTime + 1;
+					SetTarget();
+				}
+				
+				if ( m_liPath != null )
+				{
+					if ( m_bReachedNode )
+					{
+						m_bReachedNode = false;
+						if ( m_iNodeIndex < m_liPath.Count )
+							m_v3CurrNode = m_liPath[m_iNodeIndex];
+					} 
+                    else
+						GoTo();
+				}
+				break;
+			}
+		}
+    }
+	
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               GoTo
+    /////////////////////////////////////////////////////////////////////////////
+	void GoTo()
+	{
+		if ( DebugMode )
+		{
+			for ( int i=0; i<m_liPath.Count-1; ++i )
+			{
+				Debug.DrawLine( ( Vector3 )m_liPath[ i ], ( Vector3 )m_liPath[ i + 1 ], Color.white, 0.01f );
+			}
+		}
+		
+		Vector3 v3NewPos = transform.position;
+		float fXdistance = v3NewPos.x - m_v3CurrNode.x;
+        float fYdistance = v3NewPos.z - m_v3CurrNode.z;
+
+		if ( fXdistance < 0 ) 
+            fXdistance -= fXdistance * 2;
+
+		if ( fYdistance < 0 ) 
+            fYdistance -= fYdistance * 2;
+	
+		if ( ( fXdistance < 0.1 && fYdistance < 0.1 ) && m_v3Target == m_v3CurrNode )
+		{
+			m_eState = EDroneState.DRONE_IDLE;
+		}
+
+		else if ( fXdistance < 0.1 && fYdistance < 0.1 )
+		{
+			m_iNodeIndex++;
+			m_bReachedNode = true;
+		}
+
+		Vector3 v3Motion = m_v3CurrNode - v3NewPos;
+		v3Motion.Normalize();
+		v3NewPos += v3Motion * m_fSpeed;
+		
+		transform.position = v3NewPos;
+	}
+	
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               SetTarget
+    /////////////////////////////////////////////////////////////////////////////
+	private void SetTarget()
+	{
+		m_liPath = CNodeController.FindPath( transform.position, m_v3Target );
+		m_iNodeIndex = 0;
+		m_bReachedNode = true;
+	}
+	
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               MoveOrder
+    /////////////////////////////////////////////////////////////////////////////
+	public void MoveOrder( Vector3 v3Pos )
+	{
+		m_v3Target = v3Pos;
+		SetTarget();
+		m_eState = EDroneState.DRONE_MOVING;
+	}
+}
---
+++ Mothership/Assets/Scripts/AI/PathFinding/CNodeController.cs
@@ -0,0 +1,310 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Mothership;
+
+/////////////////////////////////////////////////////////////////////////////
+/// CLASS:               CNode
+/////////////////////////////////////////////////////////////////////////////
+class CNode
+{
+    // Depending on node type, the node controller will run different logic.
+    public enum ENodeType
+    {
+        NODE_NONE,
+        NODE_START,
+        NODE_OPEN,
+        NODE_CLOSED,
+        NODE_END,
+    }
+
+    // Will hold the node position.
+	private Vector3 m_v3Pos;
+    public Vector3 NodePosition { get { return m_v3Pos; } }
+
+    // Will hold the type of the node.
+	private ENodeType m_eNodeType = ENodeType.NODE_NONE;
+    public ENodeType NodeType { get { return m_eNodeType; } }
+
+    // Will hold the weight of this node. This is used to calculate the fastest
+    //  way through the nodes towards the target.
+	private float m_fWeight = 0;
+    public float Weight { get { return m_fWeight; } }
+
+    // Will hold a reference to the previous node.
+	private CNode m_cPrevNode;
+    public CNode PreviousNode { get { return m_cPrevNode; } }
+	
+    // Will contain a list of all the nodes to which we have a clear line of sight.
+	private List< CNode > m_liConnectedNodes = new List< CNode >();
+    public List< CNode > ConnectedNodes { get { return m_liConnectedNodes; } }
+
+
+	private List< CNode > m_liPotentialPrevPoints = new List< CNode >();
+    public List<CNode> PotentialPrevNodes { get { return m_liPotentialPrevPoints; } }
+		
+    /////////////////////////////////////////////////////////////////////////////
+    /// CTOR:               CNode
+    /////////////////////////////////////////////////////////////////////////////
+	public CNode( Vector3 v3Pos, ENodeType eType = ENodeType.NODE_NONE )
+	{
+        // Set up the node.
+		m_v3Pos = v3Pos;
+		m_eNodeType = eType;
+	}
+		
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               AddNode
+    /////////////////////////////////////////////////////////////////////////////		
+	public void AddNode( CNode cNode )
+	{
+		m_liConnectedNodes.Add( cNode );
+	}
+		
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               AddPrevNode
+    /////////////////////////////////////////////////////////////////////////////
+	public void AddPrevNode( CNode cNode )
+	{
+		m_liPotentialPrevPoints.Add( cNode );
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////
+    /// Function:               SetPrevNode
+    /////////////////////////////////////////////////////////////////////////////
+	public void SetPrevNode(CNode cNode)
+	{
+		m_cPrevNode = cNode;
+	}
+		
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               SetState
+    /////////////////////////////////////////////////////////////////////////////
+	public void SetState( ENodeType eType )
+	{
+		m_eNodeType = eType;
+	}
+		
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               SetWeight
+    /////////////////////////////////////////////////////////////////////////////
+	public void SetWeight( float fWeight )
+	{
+		m_fWeight = fWeight;
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////
+/// CLASS:               CNodeController
+/////////////////////////////////////////////////////////////////////////////
+public class CNodeController : MonoBehaviour {
+	
+    /////////////////////////////////////////////////////////////////////////////
+    /// Function:               FindPath
+    /////////////////////////////////////////////////////////////////////////////
+	public static List< Vector3 > FindPath( Vector3 v3StartPos, Vector3 v3TargetPos )
+	{
+        // For error reporting.
+        string strFunction = "CNodeController::FindPath()";
+
+		// We need to check if we have a clear path towards the target.
+		float fTargetDistance = Vector3.Distance( v3StartPos, v3TargetPos );
+		//if ( fTargetDistance > .7f )
+			//fTargetDistance -= .7f;
+		if ( false == Physics.Raycast( v3StartPos, v3TargetPos - v3StartPos, fTargetDistance ) )
+		{
+            // There's nothing standing between the start position and the target,
+            //  it's a straight forward path so we can set our NPC on his merry way.
+			List< Vector3 > liPath = new List<Vector3>();
+
+            // Add the start and target nodes to the path and return it.
+			liPath.Add(v3StartPos);
+			liPath.Add(v3TargetPos);
+
+			return liPath;
+		}
+		
+        // Retrieve all gameobjects with the node tag and create a node list using their positions.
+		GameObject[] rggoNodes = GameObject.FindGameObjectsWithTag( Tags.TAG_NODE );
+		List< CNode > liNodes = new List< CNode >();
+		foreach ( GameObject goNode in rggoNodes )
+		{
+            // Create and add the node to the list.
+			CNode cCurrNode = new CNode( goNode.transform.position );
+			liNodes.Add( cCurrNode );
+		}
+		
+        // Create a separate node for the start and target node.
+        CNode cStartNode = new CNode( v3StartPos, CNode.ENodeType.NODE_START );
+		CNode cTargetNode = new CNode( v3TargetPos, CNode.ENodeType.NODE_END );
+		
+        // Declare variables here for reuse inside the loops.
+        float fDistance = 0;
+
+		// Create the node connections for all our nodes.
+		foreach( CNode cNode1 in liNodes )
+		{
+			foreach ( CNode cNode2 in liNodes )
+			{
+                // Get the distance between the two nodes and check if there's anything standing between
+				fDistance = Vector3.Distance( cNode1.NodePosition, cNode2.NodePosition );
+				if ( false == Physics.Raycast( cNode1.NodePosition, cNode2.NodePosition - cNode1.NodePosition, fDistance ) )
+				{
+					Debug.DrawRay( cNode1.NodePosition, cNode2.NodePosition - cNode1.NodePosition, Color.white, 1);
+					cNode1.AddNode( cNode2 );
+				}
+			}
+
+            // Check if we can get to the target node.
+			fDistance = Vector3.Distance( v3TargetPos, cNode1.NodePosition );
+			if ( false == Physics.Raycast( v3TargetPos, cNode1.NodePosition - v3TargetPos, fDistance ) )
+			{
+				Debug.DrawRay( v3TargetPos, cNode1.NodePosition - v3TargetPos, Color.white, 1 );
+				cNode1.AddNode( cTargetNode );
+			}
+		}
+		
+		// Loop through a the node list and find all nodes which the NPC can use to travel to from the start node.
+		foreach ( CNode cNode in liNodes )
+		{
+            // Get the distance between the current node and the start position and cast a ray to see if we
+            //  have any obstacles in the way.
+			fDistance = Vector3.Distance( v3StartPos, cNode.NodePosition );
+			if (false == Physics.Raycast( v3StartPos, cNode.NodePosition - v3StartPos, fDistance ) )
+			{
+				// There's nothing in the way, we can travel to this node if we choose to.
+                Debug.DrawRay( v3StartPos, cNode.NodePosition - v3StartPos, Color.white, 1);
+				
+                // Set the previous node to the start node.
+				cNode.SetPrevNode( cStartNode );
+
+                // Indicate that this node is open and we can use it.
+				cNode.SetState( CNode.ENodeType.NODE_OPEN );
+
+                // Use the total distance from the start position to the end node as weight.
+				cNode.SetWeight( fDistance + Vector3.Distance( v3TargetPos, cNode.NodePosition ) );
+			}
+		}
+		
+		bool bSearchedAll = false;
+		bool bFoundEnd = false;
+		
+		while( false == bSearchedAll )
+		{
+            // Searched for all flag will be true unless there are open nodes in the nodelist.
+			bSearchedAll = true;
+
+			foreach ( CNode cNode in liNodes )
+			{
+				if ( cNode.NodeType == CNode.ENodeType.NODE_OPEN )
+				{
+                    // Indicate that there are still open nodes
+					bSearchedAll = false;
+
+                    // Get a list of all connected nodes.
+					List< CNode > liConnectedNodes = cNode.ConnectedNodes;
+					foreach ( CNode cPotentialNode in liConnectedNodes )
+					{
+                        switch ( cPotentialNode.NodeType )
+                        {
+                            case CNode.ENodeType.NODE_NONE:
+
+                                // Add our current node as the previous node of the potential node.
+                                cPotentialNode.AddPrevNode(cNode);
+
+                                // Set the node to open.
+                                cPotentialNode.SetState( CNode.ENodeType.NODE_OPEN );
+
+                                // Set the node weight.
+							    cPotentialNode.SetWeight( Vector3.Distance( v3StartPos, cPotentialNode.NodePosition ) + Vector3.Distance( v3TargetPos, cPotentialNode.NodePosition ) );
+
+                                break;
+
+                            case CNode.ENodeType.NODE_END:
+
+                                // We found the exit.
+							    bFoundEnd = true;
+							    cTargetNode.AddNode( cNode );
+
+                                break;
+                        }
+					}
+
+                    // Set this node to closed so we don't reuse it.
+					cNode.SetState( CNode.ENodeType.NODE_CLOSED );
+				}
+			}
+		}
+		
+        // Check if we found the end node.
+		if ( bFoundEnd )
+		{
+            // This list will hold the shortest path to the target.
+            List< CNode > liShortestPath = null;
+
+            // Will hold the lowest weight.
+			float fLowestWeight = -1;
+
+			// Trace back finding the route with the least weight.
+			foreach ( CNode cNode in cTargetNode.ConnectedNodes )
+			{
+                // We're holding a separate reference to cNode so we can trace back
+                //  without modifying cNode's value.
+                CNode cCurrentNode = cNode;
+				float fWeight = 0;
+				bool bTracing = true;
+				
+                // Will hold the full path after we're done tracing.
+				List< CNode > liPath = new List< CNode >();
+				liPath.Add(cTargetNode);
+
+				while( bTracing )
+				{
+					liPath.Add( cCurrentNode );
+					if ( cCurrentNode.NodeType == CNode.ENodeType.NODE_START )
+					{
+                        // Lowest weight hasn't been set yet, set it now.
+						if ( -1 == fLowestWeight )
+						{
+							liShortestPath = liPath;
+							fLowestWeight = fWeight;
+						} 
+
+                        else
+						{
+                            // Check if we found a faster path.
+							if ( fLowestWeight > fWeight )
+							{
+								liShortestPath = liPath;
+								fLowestWeight = fWeight;
+							}
+						}
+						bTracing = false;
+						break;
+					}
+
+                    // Add the current node's weight to the total weight of this path
+                    //  and find the next previous node.
+					fWeight += cCurrentNode.Weight;
+					cCurrentNode = cCurrentNode.PreviousNode;
+				}
+			}
+			
+            // Reverse the path and return a list of vectors to the caller.
+			liShortestPath.Reverse();
+			List< Vector3 > liWantedPath = new List< Vector3 >();
+			foreach ( CNode cNode in liShortestPath )
+			{
+				liWantedPath.Add( cNode.NodePosition );
+			}
+			return liWantedPath;
+		}
+        else
+		{
+            // We did not manage to find a path, report the issue and return null
+            Debug.LogError( string.Format( "{0} {1}", strFunction, ErrorStrings.ERROR_PATHFINDING_NO_VALID_PATH ) );
+			return null;
+		}
+	}
+}
