260d41e0c298f08253d74e5b1f71b452b5729373
AyARL <ayamrau@gmail.com>
2014-12-14 13:36:40 +0000

    Server Quit Option

--- Mothership/Assets/Scripts/UI/ServerLobbyGUI.cs
+++ Mothership/Assets/Scripts/UI/ServerLobbyGUI.cs
@@ -10,21 +10,23 @@ namespace MothershipUI
         [SerializeField]
         private GameObject content = null;
         [SerializeField]
-        private GameObject clientList = null;
-        [SerializeField]
-        private Text clientLabelPrefab = null;
-        private Text[] clientLabels = null;
+        private Button quitButton = null;
+        //private GameObject clientList = null;
+        //[SerializeField]
+        //private Text clientLabelPrefab = null;
+        //private Text[] clientLabels = null;
 
         public void Enable()
         {
             content.SetActive(true);
 
-            clientLabels = new Text[Network.maxConnections];
-            for (int i = 0; i < clientLabels.Length; i++)
-            {
-                Text t = Instantiate(clientLabelPrefab) as Text;
-                t.transform.SetParent(clientList.transform);
-            }
+            quitButton.onClick.AddListener(CloseServer);
+            //clientLabels = new Text[Network.maxConnections];
+            //for (int i = 0; i < clientLabels.Length; i++)
+            //{
+            //    Text t = Instantiate(clientLabelPrefab) as Text;
+            //    t.transform.SetParent(clientList.transform);
+            //}
         }
 
         public void Disable()
@@ -41,8 +43,11 @@ namespace MothershipUI
         {
             if (NetworkManager.networkManager != null)
             {
+                MasterServer.UnregisterHost();
                 NetworkManager.networkManager.RemoveNetworkManager();
             }
+
+            Application.Quit();
         }
     }
 }
